02f07d579afb6ec016945f3a7b76b15d0e2a7693
Hamed042gh <blearn704@gmail.com>
2024-08-17 21:07:56 +0330

    Delete auth system for change it to Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -9,27 +9,20 @@ use App\Http\Controllers\PanelController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\Auth\AuthController;
-use App\Http\Controllers\Auth\SocialAuthController;
 use App\Http\Controllers\Auth\PasswordController;
-use App\Http\Controllers\Auth\ForgotController;
+use App\Http\Controllers\Auth\SocialAuthController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 
-Route::get('/register', [AuthController::class, 'registerForm'])->name('registerForm');
-Route::post('/register', [AuthController::class, 'register'])->name('register');
-Route::get('/login', [AuthController::class, 'loginForm'])->name('loginForm');
-Route::post('/login', [AuthController::class, 'login'])->middleware('throttle')->name('login');
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
 Route::get('auth/{provider}/redirect', [SocialAuthController::class, 'redirectToProvider'])->name('social.redirect');
 Route::get('auth/{provider}/callback', [SocialAuthController::class, 'handleProviderCallback'])->name('social.callback');
 
-
-Route::get('/change-password',[ForgotController::class,'forgotpasswordForm'])->name('forget-password');
-Route::post('/change-password',[ForgotController::class,'checkingEmail'])->name('checkemail');
-Route::post('password/email', [PasswordController::class, 'sendResetLinkEmail']);
-
 Route::resource('posts', PostController::class)
     ->middleware('auth')
     ->except(['index', 'show']);
@@ -45,3 +38,5 @@ Route::resource('profile', ProfileController::class)
 Route::resource('profile', ProfileController::class)
     ->only(['index', 'show']);
 
+
+    
